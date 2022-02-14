b8c2f084971c4d43b0b6429b0d532384391de604
qxqxia <96128825+qxqxia@users.noreply.github.com>
2022-02-14 23:01:21 +0100

    Add files via upload

--- union/union.c
+++ union/union.c
@@ -1,23 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   union.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: qxia <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/07 14:46:01 by qxia              #+#    #+#             */
-/*   Updated: 2022/02/07 17:57:16 by qxia             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-//判断是否有重复
-//先判断s1 再判断s2  s2里面再判断有没有跟s1相同的 然后打印
-
 #include <unistd.h>
 
-int	ft_check_char(char *str, char c, int index)
+int ft_check_char(char *str, char c, int index)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (i < index)
@@ -29,16 +14,16 @@ int	ft_check_char(char *str, char c, int index)
 	return (1);
 }
 
-void	ft_union(char *s1, char *s2)
+void ft_union(char *s1, char *s2)
 {
-	int	i;
-	int	len;
+	int i;
+	int len;
 
 	i = 0;
 	while (s1[i])
 	{
 		if (ft_check_char(s1, s1[i], i) == 1)
-			write (1, &s1[i], 1);
+			write(1, &s1[i], 1);
 		i++;
 	}
 	len = i;
@@ -48,18 +33,16 @@ void	ft_union(char *s1, char *s2)
 		if (ft_check_char(s2, s2[i], i) == 1)
 		{
 			if (ft_check_char(s1, s2[i], len) == 1)
-				write (1, &s2[i], 1);
-			i++;
+				write(1, &s2[i], 1);
 		}
+		i++;
 	}
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	if (argc == 3)
 		ft_union(argv[1], argv[2]);
 	write(1, "\n", 1);
 	return (0);
 }
-
-
